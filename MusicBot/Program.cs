b5934b476fb7d8bb472d1beb62578653fda2da4a
vesko662 <vesko662@abv.bg>
2021-09-06 14:53:10 +0300

    Add project files.

---
+++ MusicBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MusicBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

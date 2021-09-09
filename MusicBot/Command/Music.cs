c62307fa774bd93f3f772b5e636f75ad24fb395a
vesko662 <vesko662@abv.bg>
2021-09-09 16:34:27 +0300

    Implemented leave, skip, pause, resume.

--- MusicBot/Command/Music.cs
+++ MusicBot/Command/Music.cs
@@ -1,5 +1,6 @@
 ﻿using Discord;
 using Discord.Commands;
+using Discord.WebSocket;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,6 +17,8 @@ namespace MusicBot.Command
         {
             this.lavaNode = lavaNode;
         }
+
+        
 
         [Command("join")]
         public async Task Join()
@@ -46,19 +49,48 @@ namespace MusicBot.Command
 
         }
 
+        [Command("leave")]
+        public async Task Leave()
+        {
+            if (!lavaNode.HasPlayer(Context.Guild))
+            {
+                await ReplyAsync("I'm not connected to a voice channel!");
+                return;
+            }
+
+            var voiceState = Context.User as IVoiceState;
+
+            if (voiceState?.VoiceChannel is null)
+            {
+                await ReplyAsync("You are not connected to a voice channel!");
+                return;
+            }
+
+            try
+            {
+                await lavaNode.LeaveAsync(voiceState.VoiceChannel);
+            }
+            catch (Exception exception)
+            {
+                await ReplyAsync(exception.Message);
+            }
+
+        }
+
         [Command("p")]
         public async Task Play([Remainder] string searchQuery = null)
         {
+            if (!lavaNode.HasPlayer(Context.Guild))
+            {
+                await Join();
+            }
+
             if (string.IsNullOrWhiteSpace(searchQuery))
             {
                 await ReplyAsync("Please provide search terms.");
                 return;
             }
 
-            if (!lavaNode.HasPlayer(Context.Guild))
-            {
-                await Join();
-            }
 
             var searchResponse = await lavaNode.SearchYouTubeAsync(searchQuery);
 
@@ -76,7 +108,114 @@ namespace MusicBot.Command
                 await player.PlayAsync(track);
                 await ReplyAsync($"Now Playing: {track.Title}");
             }
+        }
 
+        [Command("skip")]
+        public async Task Skip()
+        {
+            var voiceState = Context.User as IVoiceState;
+
+            if (voiceState?.VoiceChannel is null)
+            {
+                await ReplyAsync("You must be connected to a voice channel!");
+                return;
+            }
+
+            if (!lavaNode.HasPlayer(Context.Guild))
+            {
+                await ReplyAsync("You are not connected to a voice channel!");
+                return;
+            }
+
+            var player = lavaNode.GetPlayer(Context.Guild);
+
+            if (voiceState.VoiceChannel != player.VoiceChannel)
+            {
+                await ReplyAsync("You need to be in the same voice channel as me!");
+                return;
+            }
+
+            if (player.Queue.Count == 0)
+            {
+                await ReplyAsync("There are no more song in the queue!");
+                return;
+            }
+
+            await player.SkipAsync();
+
+            await ReplyAsync($"Now Playing: {player.Track.Title}");
+        }
+
+        [Command("pause")]
+        public async Task Pause()
+        {
+            var voiceState = Context.User as IVoiceState;
+
+            if (voiceState?.VoiceChannel is null)
+            {
+                await ReplyAsync("You must be connected to a voice channel!");
+                return;
+            }
+
+            if (!lavaNode.HasPlayer(Context.Guild))
+            {
+                await ReplyAsync("You are not connected to a voice channel!");
+                return;
+            }
+
+            var player = lavaNode.GetPlayer(Context.Guild);
+
+            if (voiceState.VoiceChannel != player.VoiceChannel)
+            {
+                await ReplyAsync("You need to be in the same voice channel as me!");
+                return;
+            }
+
+            if (player.PlayerState == PlayerState.Paused || player.PlayerState == PlayerState.Stopped)
+            {
+                await ReplyAsync("The music is already paused!");
+                return;
+            }
+
+            await player.PauseAsync();
+
+            await ReplyAsync("Paused the music!");
+        }
+
+        [Command("resume")]
+        public async Task Resume()
+        {
+            var voiceState = Context.User as IVoiceState;
+
+            if (voiceState?.VoiceChannel is null)
+            {
+                await ReplyAsync("You must be connected to a voice channel!");
+                return;
+            }
+
+            if (!lavaNode.HasPlayer(Context.Guild))
+            {
+                await ReplyAsync("You are not connected to a voice channel!");
+                return;
+            }
+
+            var player = lavaNode.GetPlayer(Context.Guild);
+
+            if (voiceState.VoiceChannel != player.VoiceChannel)
+            {
+                await ReplyAsync("You need to be in the same voice channel as me!");
+                return;
+            }
+
+            if (player.PlayerState == PlayerState.Playing)
+            {
+                await ReplyAsync("The music is already playing!");
+                return;
+            }
+
+            await player.ResumeAsync();
+
+            await ReplyAsync("Resumed the music!");
         }
     }
 }
